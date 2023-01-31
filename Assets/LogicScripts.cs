30ee6605ef574f6e904f14ed21e48f7fe400f2e7
Winston Grant <winstonf.grant@hotmail.com>
2023-01-30 22:25:00 -0800

    finally uploading this half baked master pieces lol

---
+++ Assets/LogicScripts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class LogicScripts : MonoBehaviour
+{
+
+    public int playerScore;
+    public TMP_Text scoreText;
+
+    [ContextMenu("increase score")]
+    public void addScore(){
+        playerScore += 1;
+        scoreText.text = playerScore.ToString();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FoxScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxScript : MonoBehaviour
+{
+
+    public Rigidbody2D rg2D;
+
+    public float jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) == true){
+
+
+            rg2D.velocity = Vector2.up * jump;
+        
+        }
+    }
+}
